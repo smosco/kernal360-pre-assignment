7d53b3545ee7448bde3b136b9fa42731a38fa232
smosco <go1ruf2tk3@gmail.com>
2024-09-03 10:49:33 +0900

    design: dashboard 레이아웃 구현

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,20 @@
+import { DashboardContainer, ContentContainer } from './styles';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Header from '../../components/Header/Header';
+import RecentOrders from '../../components/RecentOrders/RecentOrders';
+import OrderList from '../../components/OrderList/OrderList';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <DashboardContainer>
+      <Sidebar />
+      <ContentContainer>
+        <Header />
+        <RecentOrders />
+        <OrderList />
+      </ContentContainer>
+    </DashboardContainer>
+  );
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const DashboardContainer = styled.div`
+  display: flex;
+`;
+
+export const ContentContainer = styled.div`
+  flex-grow: 1;
+  background-color: #f5f5f6;
+  padding: 3rem 3.6rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2.6rem;
+`;
